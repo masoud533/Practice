fbf79dab51c09487b9c30c0b5664823a76b67b1b
masoud533 <mr.masoud_mapar@outlook.com>
2022-10-04 21:28:07 +0330

    some change in shift function

--- shift.py
+++ shift.py
@@ -1,4 +1,5 @@
 def shift(l: list = [0,1,2,3,4],i: int =2, x: str = 'Right') -> list:
+    i %= len(l)
     if x == 'Right' or x == 'right' or x == 'r' and i >= 0:
         return l[-i::] + l[:-i:]
     elif x == 'Left' or x == 'left' or x == 'l' and i >= 0:
@@ -7,4 +8,4 @@ def shift(l: list = [0,1,2,3,4],i: int =2, x: str = 'Right') -> list:
         return l[i::] + l[:i:]
     else:
         return 'ops! something went wrong, please try again'
-print(shift(x='r', i=1))
+print(shift(x='l', i=12))
