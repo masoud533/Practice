431235d848082c52d0fdc0477be5c98d2e6eabe6
masoud533 <mr.masoud_mapar@outlook.com>
2023-07-31 19:28:29 +0330

    modified oop002 python file

--- oop/oop002.py
+++ oop/oop002.py
@@ -6,6 +6,12 @@ class User:
     def __init__(self,username: str) -> None:
         self.userName = username
         self._email = "mr.masoud_mapar@outlook.com"
+    
+    def login(self, gotPassword):
+        if self._password == gotPassword:
+            print("logon user")
+        else:
+            print("you are not logged in")
 
 
 me = User('masoud')
