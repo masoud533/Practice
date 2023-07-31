e132568909366ed7cd8adb89d03ee87462e6c3a8
masoud533 <mr.masoud_mapar@outlook.com>
2023-07-31 21:32:14 +0330

    complet bullets selecte function related GunProblem

--- oop/GunProblemSample/Shooter.py
+++ oop/GunProblemSample/Shooter.py
@@ -1,4 +1,7 @@
 class Shooter:
+    def __init__(self) -> None:
+        self.selectedGun = None
+        self.selectedBullet = None
     def set_gun_by_name(self, name: str) -> None:
         Guns = {
             "Submachine Gun" : {"range":100, "power":10, "bullet size": 0.5},
@@ -14,15 +17,26 @@ class Shooter:
 
     def add_bullet_of_given_size_to_gun(self, size: float, count: int) -> None:
         Bullets = {
-            'A': {"size":0.5, "dmage":1, "count":10},
-            'B': {"size":1, "dmage":1.5, "count":10},
-            'C': {"size":3, "dmage":3, "count":10},
-            'D': {"size":4, "dmage":2, "count":10}}
-
+            0.5: {"size":0.5, "dmage":1, "count":10},
+            1.0: {"size":1, "dmage":1.5, "count":10},
+            3.0: {"size":3, "dmage":3, "count":10},
+            4.0: {"size":4, "dmage":2, "count":10}}
+        if self.selectedGun == None:
+            raise Exception("please select your gun first!")
+        elif self.selectedGun['bullet size'] != size:
+            raise Exception("Please pay attention to the selected bullet size!")
+        elif size not in list(Bullets.keys()):
+            raise Exception("we don't have this bullet")
+        elif Bullets[size]["count"] - count <= -1:
+            raise Exception("We don't have enough bullets")
+        else:
+            self.selectedBullet = Bullets[size]
+            Bullets[size]["count"] -= Bullets[size]["count"] - count 
     def shoot_to_target(self, target_x: int,  target_y: int,  target_distance: int,  aim_x: int,  aim_y: int) -> float:
         pass
 
 
 Submachine = Shooter()
 
-Submachine.set_gun_by_name('Submachine gun')
+Submachine.set_gun_by_name('Submachine Gun')
+Submachine.add_bullet_of_given_size_to_gun(0.5, 5)
