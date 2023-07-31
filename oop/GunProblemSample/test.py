8f4bf32c9999f1ef2d55cde3ae660dca903d705d
masoud533 <mr.masoud_mapar@outlook.com>
2023-07-31 21:13:26 +0330

    some test for review

--- oop/GunProblemSample/test.py
+++ oop/GunProblemSample/test.py
@@ -4,6 +4,6 @@ Guns = {
             "Pistol" : {"range":80, "power":8, "bullet size": 0.5},
             "Shotgun" : {"range":50, "power":40, "bullet size": 4},
             "Sniper Rifle" : {"range":1000, "power":30, "bullet size": 3}}
-
-print("Submachine gun" in list(Guns.keys()))
+gun = list(guns for guns in list(Guns.keys()) if Guns[guns]["range"] == 80)
+print(gun)
 print(Guns["Submachine Gun"])
