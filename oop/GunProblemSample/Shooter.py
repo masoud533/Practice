36b51fc5f3db8485e40ac08c44b255d5c2d5d5b8
masoud533 <mr.masoud_mapar@outlook.com>
2023-07-31 19:54:09 +0330

    statrt GunProblemSample Practice

---
+++ oop/GunProblemSample/Shooter.py
@@ -0,0 +1,14 @@
+class Shooter:
+    def set_gun_by_name(self, name: str) -> None:
+        Guns = {
+            "Submachine Gun" : {"range":100, "power":10, "bullet size": 0.5},
+            "Assault Rifle" : {"range":200, "power":20, "bullet size": 1},
+            "Pistol" : {"range":80, "power":8, "bullet size": 0.5},
+            "Shotgun" : {"range":50, "power":40, "bullet size": 4},
+            "Sniper Rifle" : {"range":1000, "power":30, "bullet size": 3}}
+
+    def add_bullet_of_given_size_to_gun(self, size: float, count: int) -> None:
+        pass
+
+    def shoot_to_target(self, target_x: int,  target_y: int,  target_distance: int,  aim_x: int,  aim_y: int) -> float:
+        pass
---
+++ oop/GunProblemSample/test.py
@@ -0,0 +1,1 @@
+print('Submachine Gun' == "Submachine Gun")
